2a70e124948f4b4576c754e48c6f08a5151ee157
TairMalul <118104637+TairMalul@users.noreply.github.com>
2024-03-28 14:25:09 +0200

    Update Program.cs
    improved version

--- twiter_mission/Program.cs
+++ twiter_mission/Program.cs
@@ -2,16 +2,29 @@ using System;
 using System.Security.Cryptography.X509Certificates;
 
 int option=0;
+int height = 0;
+int width = 0;
 
-Console.WriteLine("Enter 1 for Rectangle, 2 For Triangle ans 3 to Exit");
-option = Int32.Parse(Console.ReadLine()!);
-
+Console.WriteLine("Enter 1 for Rectangle, 2 For Triangle or 3 to Exit");
+try
+{
+    option = Int32.Parse(Console.ReadLine()!);
+} catch {
+    Console.WriteLine("Wrong input type");
+}
 while (option!=3)
 {
-    Console.WriteLine("Enter width:");
-    int width = Int32.Parse(Console.ReadLine()!);
-    Console.WriteLine("Enter Height:");
-    int height = Int32.Parse(Console.ReadLine()!);
+    if (option == 1 || option == 2)
+    {
+        try
+        {
+            Console.WriteLine("Enter width:");
+            width = Int32.Parse(Console.ReadLine()!);
+            Console.WriteLine("Enter Height:");
+            height = Int32.Parse(Console.ReadLine()!);
+        }
+        catch { Console.WriteLine("wront input type"); }
+    }
     if (option == 1)
     {
         Console.WriteLine("Rectangle");
@@ -21,56 +34,86 @@ while (option!=3)
     }
     else if (option == 2)
     {
+        option = 0;
         do
         {
             Console.WriteLine("Enter 1 for Triangle Perimeter or 2 to Print the Triangle");
-            option = Int32.Parse(Console.ReadLine()!);
+            try
+            {
+                option = Int32.Parse(Console.ReadLine()!);
+            }
+            catch
+            {
+                Console.WriteLine("wront input type");
+            }
         }
         while (option != 1 && option != 2);
         if (option == 1)
-            Console.WriteLine("Perimeter: " + (width + 2 * (Math.Sqrt(Math.Pow(Convert.ToDouble(width/2), 2) + Math.Pow(height, 2)))));
-        else if (width % 2 == 0 || width - 2 * height > 0)
-            Console.WriteLine("Unable to Print Triangle");
-        else
-        {
-            if(width-2*height < 0)
+            Console.WriteLine("Perimeter: " + (width + 2 * (Math.Sqrt(Math.Pow(Convert.ToDouble(width) / 2, 2) + Math.Pow(height, 2)))));
+        else 
+            if (width % 2 == 0 || width - 2 * height > 0)
+                Console.WriteLine("Unable to Print Triangle");
+            else
             {
-                string line = "";
-                //first line
-                for (int i = 0; i < width / 2; i++)
-                    line += "";
-                Console.WriteLine(line+"*");
-                //middle lines
-                int len = 1;
-                for (int i = 0;i < (width-2)/2; i++)
+                if (width - 2 * height < 0)
                 {
-                    len += 2;
-                    if(i==0)
+                    string line = "";
+                    //first line
+                    for (int i = 0; i < width / 2; i++)
+                        line += " ";
+                    Console.WriteLine(line + "*");
+                    //middle lines
+                    int len = 1;
+                    if (((width - 2) / 2) == 0)
                     {
-                        for (int j = 0; j < (height - 2) / ((width - 2) / 2)+ (height - 2) % ((width - 2) / 2); j++)
+                        for (int i = 0; i < (height - 2); i++)
                         {
                             line = "";
-                            for(int k = 0;k<len;k++)
-                            line += "*";
+                            for (int j = 0; j < width; j++)
+                                line += "*";
                             Console.WriteLine(line);
                         }
                     }
-                    else for (int j = 0; j < (height-2)/((width-2)/2); j++)
-                    {
-                        line = "";
-                        for(int k = 0;k<len;k++)
+                    else for (int i = 0; i < (width - 2) / 2; i++)
+                        {
+                            len += 2;
+                            if (i == 0)
+                            {
+                                for (int j = 0; j < (height - 2) / ((width - 2) / 2) + (height - 2) % ((width - 2) / 2); j++)
+                                {
+                                    line = "";
+                                    for (int k = 0; k < (width - len) / 2; k++)
+                                        line += " ";
+                                    for (int k = 0; k < len; k++)
+                                        line += "*";
+                                    Console.WriteLine(line);
+                                }
+                            }
+                            else for (int j = 0; j < (height - 2) / ((width - 2) / 2); j++)
+                                {
+                                    line = "";
+                                    for (int k = 0; k < (width - len) / 2; k++)
+                                        line += " ";
+                                    for (int k = 0; k < len; k++)
+                                        line += "*";
+                                    Console.WriteLine(line);
+                                }
+                        }
+                    line = "";
+                    for (int i = 0; i < width; i++)
                         line += "*";
-                        Console.WriteLine(line);
-                    }
+                    Console.WriteLine(line);
                 }
-                line = "";
-                for (int i = 0; i < width; i++)
-                    line +="*";
-                Console.WriteLine(line);
             }
-        }
     }
     Console.WriteLine("Enter 1 for Rectangle, 2 For Triangle ans 3 to Exit");
-    option = Int32.Parse(Console.ReadLine()!);
+    try
+    {
+        option = Int32.Parse(Console.ReadLine()!);
+    }
+    catch
+    {
+        Console.WriteLine("Wrong input");
+    }
 }
 Console.WriteLine("GoodBye!");
